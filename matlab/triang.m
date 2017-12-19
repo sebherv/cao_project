12d06328142b6b603ff997fa989ecf784a832119
DESKTOP-HSJD6J9\anais <anaischamb@hotmail.fr>
2017-12-19 13:32:53 +0100

    génération du fichier de données triang

--- matlab/triang.m
+++ matlab/triang.m
@@ -15,3 +15,8 @@ TRI=delaunay(sortedPoints(:,2:3));
 
 % Afficher le r√©sultat
 triplot(TRI,sortedPoints(:,2), sortedPoints(:,3));
+
+V=(1:size(TRI))';
+gen_TRI=[V TRI]; %ajout du vecteur V des index des triangles k ‡ gauche de TRI 
+
+ dlmwrite('HCT.RES',gen_TRI, ' ');
