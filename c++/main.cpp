c6c3449f9c30ca495443e1c63762c6725fc72e2a
Sebastien Hervieu <shervieu@id-apps.fr>
2017-12-26 13:20:29 +0100

    Détection minX, maxX, minY, maxY

--- c++/main.cpp
+++ c++/main.cpp
@@ -19,13 +19,38 @@ int main() {
     // trier par id croissant
     sort(pointList.begin(),pointList.end());
 
+    // Trouver les coordonnées maximales
+    double minX, minY, maxX, maxY;
+    bool init_minmax = true;
+
     std::vector<point>::iterator it;
     for(it=pointList.begin(); it!=pointList.end(); it++) {
+        if(init_minmax) {
+            minX = maxX = it->get_x();
+            minY = maxY = it->get_y();
+            init_minmax = false;
+        } else {
+            if (it->get_x() < minX) {
+                minX = it->get_x();
+            } else if(it->get_x() > maxX) {
+                maxX = it->get_x();
+            }
+
+            if(it->get_y() < minY) {
+                minY = it->get_y();
+            } else if(it->get_y() > maxY) {
+                maxY = it->get_y();
+            }
+        }
         std::cout << it->get_id() << ", " << it->get_x() << ", " << it->get_y() << std::endl;
         it->set_f(Fonction_F::getf(it->get_x(),it->get_y() ));
         it->set_df_dx(Fonction_F::getf_dx(it->get_x(),it->get_y() ));
         it->set_df_dy(Fonction_F::getf_dy(it->get_x(),it->get_y() ));
     }
+
+    //Extrema
+    std::cout << "minX = " << minX << "; maxX = " << maxX << std::endl;
+    std::cout << "minY = " << minY << "; maxY = " << maxY << std::endl;
 
     // Read input triangulation
     TriangleFileReader triangleFileReader("../../matlab/HCT.RES", pointList);
